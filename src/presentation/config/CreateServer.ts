c11af75ec5f342b5884866997dd2ca8704783208
Tarek Eslam <89143141+tarek1o@users.noreply.github.com>
2024-02-12 13:11:54 +0200

    connect to db at server creation

--- src/presentation/config/CreateServer.ts
+++ src/presentation/config/CreateServer.ts
@@ -1,8 +1,10 @@
 import { Application } from 'express';
+import prisma from '../../domain/db';
 
 abstract class CreateServer {
 	private static PORT = process.env.PORT || 8080;
 	static create = (app: Application) => app.listen(this.PORT, async() => {
+		await prisma.$connect();	
 		console.log(`App is running at: http://localhost:${this.PORT} 🚀`);
 	})
 }
